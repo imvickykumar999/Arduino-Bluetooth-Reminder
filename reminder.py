b98e8c502b1a2e508c06542ac82e6b346c4b29f6
imvickykumar999 <imvickykumar999@gmail.com>
2023-05-04 17:28:38 +0530

    📂

--- reminder.py
+++ reminder.py
@@ -1,6 +1,12 @@
 
+rem = input('\nEnter your remainder : ')
+baddr = input('\nEnter device address : ')
+
+resp = '\nYou have reached your location, you asked to remind : '
+print('\nSearching for device ...')
+
 channel = 4
-baddr = 'F8:70:9F:3A:19:24'
+# baddr = 'F8:70:9F:3A:19:24' # Airdopes 141
 raddr = (baddr, channel)
 
 
@@ -16,16 +22,13 @@ def connect(raddr):
     return s
 
 
-rem = input('Enter your remainder : ')
-
 while True:
     try:
         e = connect(raddr)
         # print(e)
-        print(rem)
+        print(resp, f'"{rem}"')
         break
 
     except Exception as e:
-        # print(e)
-        print('Searching ...')
+        print(e)
         pass
