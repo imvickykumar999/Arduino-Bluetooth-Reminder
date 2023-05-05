8ae46ddd1cdde7a671ff131da0d1e5b041e1298b
imvickykumar999 <imvickykumar999@gmail.com>
2023-05-05 13:31:02 +0530

    connected wifi

--- static/nearby.py
+++ static/nearby.py
@@ -1,7 +1,37 @@
 
-channel = 4
-baddr = 'F8:70:9F:3A:19:24' # Airdopes
-raddr = (baddr, channel)
+def connected_wifi():
+    import subprocess
+    wifi = subprocess.check_output(['netsh', 'WLAN', 'show', 'interfaces'])
+    data = wifi.decode('utf-8')
+    print(data)
+
+# connected_wifi()
+
+'''
+There is 1 interface on the system:
+
+    Name                   : Wi-Fi
+    Description            : Realtek 8822CE Wireless LAN 802.11ac PCI-E NIC
+    GUID                   : 70673e8d-7ce9-4927-91b9-5fc3b44c492e
+    Physical address       : 80:91:33:c8:ed:19
+    Interface type         : Primary
+    State                  : connected
+    SSID                   : Vicky
+    BSSID                  : e8:65:d4:1d:b2:d0
+    Network type           : Infrastructure
+    Radio type             : 802.11n
+    Authentication         : WPA2-Personal
+    Cipher                 : CCMP
+    Connection mode        : Auto Connect
+    Band                   : 2.4 GHz
+    Channel                : 10
+    Receive rate (Mbps)    : 300
+    Transmit rate (Mbps)   : 300
+    Signal                 : 100%
+    Profile                : Vicky
+
+    Hosted network status  : Not available
+'''
 
 
 def discover():
@@ -13,7 +43,6 @@ def discover():
 
     for addr, name in nearby_devices:
         print(f"  {addr} - {name}")
-
 
 # discover()
 
@@ -37,9 +66,17 @@ def mac_address():
 
 # mac_address()
 
+'''
+The MAC address in formatted way is : 80:91:33:c8:ed:18
+'''
+
 
 def connect(raddr):
     import socket
+    
+    channel = 4
+    baddr = 'F8:70:9F:3A:19:24' # Airdopes
+    raddr = (baddr, channel)
 
     s = socket.socket(socket.AF_BLUETOOTH, 
                       socket.SOCK_STREAM, 
@@ -49,12 +86,11 @@ def connect(raddr):
     s.connect(raddr)
     print(s)
 
-
-try:
-    connect(raddr)
-    print('YOUR REMAINDER')
-except Exception as e:
-    print(e)
+# try:
+#     connect(raddr)
+#     print('YOUR REMAINDER')
+# except Exception as e:
+#     print(e)
 
 '''
 OUTPUT :
